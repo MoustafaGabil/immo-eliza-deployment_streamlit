12c6ef1be27c18a33fe9edf2266f2e5737ae34be
M.Gabil <127035455+mgabil@users.noreply.github.com>
2024-11-15 09:48:34 +0100

    Streamlit Api

---
+++ main.py
@@ -0,0 +1,94 @@
+import streamlit as st
+from predict import get_prediction
+
+# ------------------------------------ Streamlit App ------------------------------------
+# App title and description
+
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <h1>🏡 Property Price Predictor Over Belgium</h1>
+        <img src="https://images.emojiterra.com/google/noto-emoji/unicode-16.0/color/1024px/1f1e7-1f1ea.png" width="100">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+st.write(
+    "This API predicts the price of properties (houses or apartments) based on various features "
+    "Provide the necessary inputs below to get an estimate in parallel with the expected range."
+)
+st.markdown("""
+### Usage Instructions:
+- To get an accurate prediction, make sure to enter the correct location (locality) and zip code.
+- Input all relevant property details and submit to receive an estimated price.
+""")
+
+
+# User inputs
+locality_options = [
+    'Aalst', 'Antwerp', 'Arlon', 'Ath',
+    'Bastogne', 'Brugge', 'Brussels',
+    'Charleroi', 'Dendermonde', 'Diksmuide',
+    'Dinant', 'Eeklo', 'Gent',
+    'Halle-vilvoorde', 'Hasselt', 'Huy',
+    'Ieper', 'Kortrijk', 'Leuven',
+    'Liège', 'Maaseik', 'Marche-en-Famenne',
+    'Mechelen', 'Mons', 'Mouscron',
+    'Namur', 'Neufchâteau', 'Nivelles',
+    'Oostend', 'Oudenaarde', 'Philippeville',
+    'Roeselare', 'Sint-Niklaas', 'Soignies',
+    'Thuin', 'Tielt', 'Tongeren',
+    'Tournai', 'Turnhout', 'Verviers',
+    'Veurne', 'Virton', 'Waremme'
+]
+property_type = st.selectbox("Property Type", ["House", "Apartment"])
+locality = st.selectbox("Locality", locality_options, index=0)
+zip_code = st.number_input("Zip Code", value=9000, step=1)
+construction_year = st.number_input("Construction Year", min_value=1800, max_value=2024, value=2020)
+total_area_sqm = st.number_input("Total Area (sqm)", value=100.0, step=1.0)
+surface_land_sqm = st.number_input("Land Surface (sqm)", value=120.0, step=1.0)
+garden_sqm = st.number_input("Garden Size (sqm)", value=15.0, step=1.0)
+nbr_frontages = st.number_input("Number of Frontages", value=2, step=1)
+nbr_bedrooms = st.number_input("Number of Bedrooms", value=1, step=1)
+kitchen_type = st.selectbox("Kitchen Type",["NOT_INSTALLED", "SEMI_EQUIPPED", "USA_SEMI_EQUIPPED", "INSTALLED", "USA_HYPER_EQUIPPED"])
+building_state = st.selectbox("Building State",["TO_RESTORE", "TO_RENOVATE", "TO_BE_DONE_UP", "GOOD", "JUST_RENOVATED", "AS_NEW"])
+epc = st.selectbox("Energy Class (EPC)", ["G", "F", "E", "D", "C", "B", "A", "A+", "A++"])
+fl_double_glazing = st.radio("Double Glazing", [1, 0], format_func=lambda x: "Yes" if x == 1 else "No")
+fl_terrace = st.radio("Terrace", [1, 0], format_func=lambda x: "Yes" if x == 1 else "No")
+fl_swimming_pool = st.radio("Swimming Pool", [1, 0], format_func=lambda x: "Yes" if x == 1 else "No")
+fl_floodzone = st.radio("Flood Zone", [1, 0], format_func=lambda x: "Yes" if x == 1 else "No")
+
+# Creating a submit button
+submit_button = st.button("Predict Price")
+
+if submit_button:
+    try:
+        
+        # Construct the payload
+        property_details = {
+            "property_type": property_type,
+            "locality": locality,
+            "zip_code": zip_code,
+            "construction_year": construction_year,
+            "total_area_sqm": total_area_sqm,
+            "surface_land_sqm": surface_land_sqm,
+            "garden_sqm": garden_sqm,
+            "nbr_frontages": nbr_frontages,  # Hardcoded for now, adjust as needed
+            "nbr_bedrooms": nbr_bedrooms,
+            "kitchen_type": kitchen_type,
+            "building_state": building_state,
+            "epc": epc,
+            "fl_double_glazing": fl_double_glazing,
+            "fl_terrace": fl_terrace,
+            "fl_swimming_pool": fl_swimming_pool,
+            "fl_floodzone": fl_floodzone
+        }
+
+        # Calling prediction function 
+        prediction = get_prediction(property_details)
+        st.success(f"Predicted Price: €{prediction['predicted_price']}")
+        st.write(f"Price Range: €{prediction['predicted_price_range']['min']} - €{prediction['predicted_price_range']['max']}")
+
+    except ValueError as e:
+        st.error(str(e))  # Display validation error message
---
+++ predict.py
@@ -0,0 +1,127 @@
+import joblib
+import numpy as np
+import pandas as pd
+from geopy.geocoders import Nominatim
+from fastapi import HTTPException
+
+
+# Initialize geolocator
+geolocator = Nominatim(user_agent="property_price_predictor")
+
+# Load models and encoders
+house_model = joblib.load(r"models and encoders/XGB_Regression_HOUSE_without_outliers.pkl")
+apartment_model = joblib.load(r"models and encoders/XGB_Regression_APARTMENT_without_outliers.pkl")
+locality_encoder = joblib.load(r"models and encoders/locality_encoder.joblib")
+kitchen_encoder = joblib.load(r"models and encoders/encoder_kitchen_type.joblib")
+building_state_encoder = joblib.load(r"models and encoders/encoder_building_state.joblib")
+epc_encoder = joblib.load(r"models and encoders/encoder_epc.joblib")
+
+# MSE values for house and apartment
+MSE_VALUES = {
+    "house": 50102,
+    "apartment": 31681,
+}
+
+def get_lat_lon(zip_code: int, locality: str):
+    try:
+        location = geolocator.geocode(f"{locality}, {zip_code}")
+        if location:
+            return location.latitude, location.longitude
+        else:
+            raise HTTPException(status_code=404, detail="Location not found.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error fetching location details.")
+
+
+# Function to validate integer inputs
+def validate_int(value, field_name):
+    if not isinstance(value, int):
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid input for {field_name}. Please enter an integer value.",
+        )
+    if value <= 0:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid input for {field_name}. Value must be a positive integer.",
+        )
+    
+# Function to validate logical relationships between areas
+def validate_area_relationships(surface_land_sqm, total_area_sqm, garden_sqm):
+    if garden_sqm > surface_land_sqm or garden_sqm > total_area_sqm:
+        raise ValueError("Garden size cannot be larger than the surface land area or total area.")
+    if total_area_sqm > surface_land_sqm:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid input: Total area cannot be larger than the surface land.",) 
+################################################ which way should we write the error? #####################################
+
+
+# # Prediction function
+def get_prediction(property_details) -> dict:
+
+    validate_int(property_details["nbr_bedrooms"], "Number of Bedrooms")
+    validate_int(property_details["nbr_frontages"], "Number of Frontages")
+
+
+
+    # # Validate area relationships
+    validate_area_relationships(
+        property_details["surface_land_sqm"],
+        property_details["total_area_sqm"],
+        property_details["garden_sqm"],
+    )
+
+
+    # # Fetch latitude and longitude
+    latitude, longitude = get_lat_lon(property_details["zip_code"], property_details["locality"])
+
+    # # Encode categorical values
+    # try:
+    locality_encoded = locality_encoder.transform([[property_details["locality"]]])
+    kitchen_encoded = kitchen_encoder.transform([[property_details["kitchen_type"]]])[0][0]
+    building_encoded = building_state_encoder.transform([[property_details["building_state"]]])[0][0]
+    epc_encoded = epc_encoder.transform([[property_details["epc"]]])[0][0]
+
+    # Prepare features for prediction
+    features = {
+        "construction_year": property_details["construction_year"],
+        "total_area_sqm": property_details["total_area_sqm"],
+        "nbr_frontages": property_details["nbr_frontages"],
+        "nbr_bedrooms": property_details["nbr_bedrooms"],
+        "kitchen_type_encoded":kitchen_encoded ,
+        "building_state_encoded": building_encoded,
+        "epc_encoded": epc_encoded,
+        "garden_sqm": property_details["garden_sqm"],
+        "surface_land_sqm": property_details["surface_land_sqm"],
+        "fl_double_glazing": property_details["fl_double_glazing"],
+        "fl_terrace": property_details["fl_terrace"],
+        "fl_swimming_pool": property_details["fl_swimming_pool"],
+        "fl_floodzone": property_details['fl_floodzone'],
+        "latitude": latitude,
+        "longitude": longitude,
+        "zip_code": property_details["zip_code"],
+    }
+
+    # Convert to DataFrame and concatenate locality encoding
+    
+    features_df = pd.DataFrame([features])
+    input_data = np.concatenate([features_df.values, locality_encoded], axis=1)
+
+    # Select model based on property type
+    model = house_model if property_details["property_type"].lower() == "house" else apartment_model
+
+    # Make the prediction
+    prediction = model.predict(input_data)[0]
+    prediction = int(prediction)
+
+    # Calculate the price range
+    mse = MSE_VALUES[property_details["property_type"].lower()]
+    price_range = {"min": prediction - mse, "max": prediction + mse}
+
+    # Return the prediction in the expected format
+    return {
+        "property_type": property_details["property_type"],
+        "predicted_price": prediction,
+        "predicted_price_range": price_range
+    }
